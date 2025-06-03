7ab7aa59fe61d4b298e6209b389be3c3fe3a62e6
rhmt2492 <fc129d5y1892@student.devacademy.id>
2025-06-03 11:24:45 +0700

    Update package.json, hapus webpack dan sesuaikan 8

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,5 @@
 
 
-// require('dotenv').config(); 
-
-// const express = require('express');
-// const cors = require('cors'); 
-// const app = express();
-// const db = require('./db'); // koneksi DB
-// const authRoutes = require('./routes/authRoutes'); 
-
-// const fs = require('fs');
-// const path = require('path');
-
-// const uploadDir = path.join(__dirname, 'uploads');
-
-
-// app.use(cors({
-//   origin: 'http://localhost:8080', // 
-//   credentials: true
-// }));
-
-// app.use(express.json());
-
-// // Register route ke express
-// app.use('/api/auth', authRoutes); 
-
-
-// app.get('/', (req, res) => {
-//   res.send('Server is running');
-// });
-
-// const PORT = 8000;
-// app.listen(PORT, () => {
-//   console.log(`Server berjalan di http://localhost:${PORT}`);
-// });
-
-
-
-// if (!fs.existsSync(uploadDir)) {
-//   fs.mkdirSync(uploadDir);
-//   console.log('Folder uploads berhasil dibuat otomatis');
-// }
-
-
 require('dotenv').config(); 
 
 const express = require('express');
@@ -55,8 +13,9 @@ const path = require('path');
 
 const uploadDir = path.join(__dirname, 'uploads');
 
+
 app.use(cors({
-  origin: '*', // sesuaikan sesuai domain front-end kamu
+  origin: 'http://localhost:8080', // 
   credentials: true
 }));
 
@@ -65,14 +24,17 @@ app.use(express.json());
 // Register route ke express
 app.use('/api/auth', authRoutes); 
 
+
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
 });
+
+
 
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
