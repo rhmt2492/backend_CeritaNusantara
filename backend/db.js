0fe3ff01f8eb3513ed57d7d8968ff533db7a7f75
rhmt2492 <fc129d5y1892@student.devacademy.id>
2025-06-03 13:12:54 +0700

    Update package.json, hapus webpack dan sesuaikan 12

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,25 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+// const db = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+
+// In db.js
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
+
+
 
 db.connect((err) => {
     if (err) {
@@ -16,4 +29,5 @@ db.connect((err) => {
     }
 });
 
-module.exports = db;
+// module.exports = db;
+module.exports = pool.promise(); // Enable promise interface
