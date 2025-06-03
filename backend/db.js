d258bbcec207c169888cda8d44449c6a5be5998b
rhmt2492 <fc129d5y1892@student.devacademy.id>
2025-06-03 13:31:19 +0700

    Update package.json, hapus webpack dan sesuaikan 14

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,40 @@
+// const mysql = require('mysql2');
+// require('dotenv').config();
+
+// const db = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+
+// db.connect((err) => {
+//     if (err) {
+//         console.error('Error connecting to database:', err);
+//     } else {
+//         console.log('Connected to database');
+//     }
+// });
+
+// module.exports = db;
+
+
 const mysql = require('mysql2');
-require('dotenv').config();
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,    // bukan 'localhost'
+  port: process.env.DB_PORT,    // harus sesuai
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to database:', err);
-    } else {
-        console.log('Connected to database');
-    }
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database:', err);
+    return;
+  }
+  console.log('Database connected!');
 });
 
-module.exports = db;
+module.exports = connection;
