2d10d2d03ad28f5326a73d281f9248ab22fa8e5d
rhmt2492 <fc129d5y1892@student.devacademy.id>
2025-06-03 13:04:27 +0700

    Update package.json, hapus webpack dan sesuaikan 10

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,8 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// const db = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME
-// });
-
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT, // <- TAMBAHKAN INI
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
